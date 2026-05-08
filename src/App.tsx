b66cb335eccb6be6c41bcaead21de461b903e119
devashish113 <devashishufraul@gmail.com>
2026-05-08 23:52:17 +0530

    layout: move contact section above faq

--- src/App.tsx
+++ src/App.tsx
@@ -1327,8 +1327,8 @@ export default function App() {
           <WhyChooseUs />
           <FounderSection />
           <HowWeWork />
-          <FAQ />
           <Contact />
+          <FAQ />
         </main>
         <SecurityBadge />
         <Footer />
